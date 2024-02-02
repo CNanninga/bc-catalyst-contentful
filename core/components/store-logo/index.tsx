68b2666a3c7fd63a40aba01effe674543d80b103
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-02-02 14:08:41 -0600

    Adjust logo size

--- core/components/store-logo/index.tsx
+++ core/components/store-logo/index.tsx
@@ -18,10 +18,10 @@ export const StoreLogo = ({ data }: Props) => {
     <Image
       alt={logo.image.altText ? logo.image.altText : storeName}
       className="max-h-16 object-contain"
-      height={32}
+      height={150}
       priority
       src={logo.image.url}
-      width={155}
+      width={300}
     />
   );
 };
