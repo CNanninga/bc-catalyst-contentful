a80497c88d955baf8c5411923f9c281d6565954e
Chris Nanninga <chris.nanninga@bigcommerce.com>
2024-10-22 16:49:28 -0500

    Add Contentful integration

--- core/app/[locale]/(default)/(faceted)/category/[slug]/page-data.ts
+++ core/app/[locale]/(default)/(faceted)/category/[slug]/page-data.ts
@@ -14,6 +14,7 @@ const CategoryPageQuery = graphql(
       site {
         category(entityId: $categoryId) {
           name
+          path
           ...BreadcrumbsFragment
           seo {
             pageTitle
--- core/app/[locale]/(default)/page.tsx
+++ core/app/[locale]/(default)/page.tsx
@@ -10,6 +10,9 @@ import { ProductCardCarouselFragment } from '~/components/product-card-carousel/
 import { Slideshow } from '~/components/slideshow';
 import { LocaleType } from '~/i18n/routing';
 
+import { getCategoryContent } from '~/contentful-client/queries/get-category-content';
+import CmsContent from '~/components/cms/cms-content';
+
 const HomePageQuery = graphql(
   `
     query HomePageQuery {
@@ -47,17 +50,25 @@ export default async function Home({ params: { locale } }: Props) {
 
   const customerId = await getSessionCustomerId();
 
-  const { data } = await client.fetch({
-    document: HomePageQuery,
-    customerId,
-    fetchOptions: customerId ? { cache: 'no-store' } : { next: { revalidate } },
-  });
+  const [ homePageData, cmsContent ] = await Promise.all([
+    client.fetch({
+      document: HomePageQuery,
+      customerId,
+      fetchOptions: customerId ? { cache: 'no-store' } : { next: { revalidate } },
+    }),
+    getCategoryContent('home', 'home'),
+  ]);
+
+  const { data } = homePageData;
 
   const featuredProducts = removeEdgesAndNodes(data.site.featuredProducts);
   const newestProducts = removeEdgesAndNodes(data.site.newestProducts);
 
   return (
     <>
+
+      {cmsContent.length > 0 && <CmsContent blocks={cmsContent} className="mx-8" />}
+
       <Slideshow />
 
       <div className="my-10">
