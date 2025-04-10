b64a9ac334f95c3f790f3a9569402a8f54c96d80
Chris Nanninga <chris.nanninga@bigcommerce.com>
2025-04-10 15:33:54 -0500

    Add Contentful content to home, category, and product pages

--- core/app/[locale]/(default)/page.tsx
+++ core/app/[locale]/(default)/page.tsx
@@ -1,6 +1,9 @@
 import { locales } from '~/i18n/locales';
 import { Page as MakeswiftPage } from '~/lib/makeswift';
 
+import { getCategoryContent } from '~/lib/contentful/client/queries/get-category-content';
+import CmsContent from '~/components/custom/contenful/cms-content';
+
 interface Params {
   locale: string;
 }
@@ -16,5 +19,12 @@ interface Props {
 export default async function Home({ params }: Props) {
   const { locale } = await params;
 
-  return <MakeswiftPage locale={locale} path="/" />;
+  const cmsContent = await getCategoryContent('home', 'home');
+
+  return (
+    <>
+      {cmsContent.length > 0 && <CmsContent blocks={cmsContent} className="mx-8" />}
+      <MakeswiftPage locale={locale} path="/" />
+    </>
+  );
 }
